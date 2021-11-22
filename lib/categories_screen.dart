b012009293ba023fc62ffd9f672663364101fcab
Harsh dalwadi <harshdalwadi500@gmail.com>
2021-11-22 12:16:14 +0530

    Fonts added successfully

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,23 +1,22 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 import 'dummy_data.dart';
+import 'category_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: DUMMY_CATEGORIES.map((category) {
-        return Container(
-          height: 100,
-          width: 100,
-          child: Text(category.title),
-        );
-      }).toList(),
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20),
+    return Scaffold(
+      appBar: AppBar(title: const Text("DeliMeals"),),
+      body: GridView(
+        padding: EdgeInsets.all(25.0),
+        children: DUMMY_CATEGORIES.map((category)=> CategoryItem(title: category.title,color: category.color,)).toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20),
+      ),
     );
   }
 }
