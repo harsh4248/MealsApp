9cd995f6a94b3fe68dba3bce567886c93be98fcf
Harsh dalwadi <harshdalwadi500@gmail.com>
2021-11-29 14:03:35 +0530

    created meals item page

--- lib/widgets/meals_item.dart
+++ lib/widgets/meals_item.dart
@@ -18,6 +18,18 @@ class MealItem extends StatelessWidget {
 
   void selectMeal() {}
 
+  String get affordibilityText {
+    if (Affordability.Affordable == afford) return "Affordable";
+    if (Affordability.Luxurious == afford) return "Luxurious";
+    if (Affordability.Pricey == afford) return "Pricey";
+  }
+
+  String get ComplexityText {
+    if (Complexity.Simple == complex) return "Simple";
+    if (Complexity.Challenging == complex) return "Challenging";
+    if (Complexity.Hard == complex) return "Hard";
+  }
+
   @override
   Widget build(BuildContext context) {
     return InkWell(
@@ -43,8 +55,57 @@ class MealItem extends StatelessWidget {
                     fit: BoxFit.cover,
                   ),
                 ),
+                Positioned(
+                  bottom: 20,
+                  right: 10,
+                  child: Container(
+                    width: 220,
+                    color: Colors.black54,
+                    padding: EdgeInsets.symmetric(vertical: 5,horizontal: 20),
+                    child: Text(
+                      title,
+                      style: TextStyle(fontSize: 26, color: Colors.white),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
+                )
               ],
-            )
+            ),
+            Padding(
+                padding: EdgeInsets.all(20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Row(
+                      children: [
+                        Icon(Icons.schedule),
+                        SizedBox(
+                          width: 6,
+                        ),
+                        Text("$duration min"),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Icon(Icons.work),
+                        SizedBox(
+                          width: 6,
+                        ),
+                        Text("$ComplexityText"),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Icon(Icons.money),
+                        SizedBox(
+                          width: 6,
+                        ),
+                        Text("$affordibilityText"),
+                      ],
+                    ),
+                  ],
+                ))
           ],
         ),
       ),
